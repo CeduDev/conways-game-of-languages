75053a573577a9db8b28c9f71b9bcca5c2f8fa7f
CeduDev <cedric.neergaard@gmail.com>
2024-06-03 22:51:30 +0300

    finalize working hardcoded blinker

--- zig/src/conway.zig
+++ zig/src/conway.zig
@@ -6,23 +6,35 @@ pub const Coordinate = struct { x: u16, y: u16 };
 
 pub const Cell = enum { DEAD, ALIVE };
 
-const ns_to_s: u32 = 1000000;
+const DELTAS: [8]struct { col: i8, row: i8 } = .{
+    .{ .row = -1, .col = -1 },
+    .{ .row = -1, .col = 0 },
+    .{ .row = -1, .col = 1 },
+    .{ .row = 0, .col = -1 },
+    .{ .row = 0, .col = 1 },
+    .{ .row = 1, .col = -1 },
+    .{ .row = 1, .col = 0 },
+    .{ .row = 1, .col = 1 },
+};
+
+const ns_to_s: u64 = 1000000;
 
 pub const Conway = struct {
+    allocator: std.mem.Allocator,
     height: u16,
     width: u16,
     text: []const u8,
     cells: []Cell,
     generations: u16,
     current_generation: u16,
-    delay: u32,
+    delay: u64,
     const Self = @This();
 
     fn clear_screen() !void {
         print("\x1B[2J\x1B[H", .{});
     }
 
-    pub fn init(allocator: std.mem.Allocator, comptime width: u16, comptime height: u16, comptime init_cells: []const Coordinate, text: []const u8, generations: u16, delay_in_milli_seconds: u32, cells: []Cell) !*Self {
+    pub fn init(allocator: std.mem.Allocator, width: u16, height: u16, init_cells: []const Coordinate, text: []const u8, generations: u16, delay_in_milli_seconds: u32, cells: []Cell) !*Self {
         // Set the cells from init_cells to ALIVE in cells
         for (init_cells) |c| {
             cells[c.y * height + c.x] = Cell.ALIVE;
@@ -30,12 +42,12 @@ pub const Conway = struct {
 
         const res = try allocator.create(Self);
 
-        res.* = Self{ .width = width, .height = height, .text = text, .cells = cells, .generations = generations, .current_generation = 0, .delay = delay_in_milli_seconds * ns_to_s };
+        res.* = Self{ .allocator = allocator, .width = width, .height = height, .text = text, .cells = cells, .generations = generations, .current_generation = 0, .delay = delay_in_milli_seconds * ns_to_s };
 
         return res;
     }
 
-    pub fn print_game(self: Self) void {
+    fn print_game(self: Self) void {
         try clear_screen();
 
         print("{s}, Generation: {}\n", .{ self.text, self.current_generation + 1 });
@@ -52,10 +64,80 @@ pub const Conway = struct {
         }
     }
 
-    pub fn run(self: *Self) void {
+    fn get_cells_index(self: Self, row: usize, col: usize) usize {
+        return (row * self.width + col);
+    }
+
+    fn get_live_neighors(self: Self, row: usize, col: usize) u8 {
+        var count: u8 = 0;
+
+        for (DELTAS) |d| {
+            const drow = d.row;
+            const dcol = d.col;
+
+            const r_ = @as(i8, @intCast(row));
+            const c_ = @as(i8, @intCast(col));
+
+            if (r_ + drow < 0 or
+                c_ + dcol < 0 or
+                r_ + drow > self.height - 1 or
+                c_ + dcol > self.width - 1)
+            {
+                continue;
+            }
+
+            const n_r: usize = @intCast(r_ + drow);
+            const n_c: usize = @intCast(c_ + dcol);
+
+            const n_idx = self.get_cells_index(n_r, n_c);
+            const cell = self.cells[n_idx];
+            if (cell == Cell.ALIVE) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    fn evolve(self: *Self) !void {
+        const next_generation: []Cell = try self.allocator.alloc(Cell, self.cells.len);
+        defer self.allocator.free(next_generation);
+
+        for (0..self.height) |row| {
+            for (0..self.width) |col| {
+                const idx = self.get_cells_index(row, col);
+                const cell = self.cells[idx];
+                const live_neighbors = self.get_live_neighors(row, col);
+
+                var next_cell: Cell = Cell.DEAD;
+
+                switch (cell) {
+                    Cell.ALIVE => {
+                        if (live_neighbors == 2 or live_neighbors == 3) {
+                            next_cell = Cell.ALIVE;
+                        }
+                    },
+                    Cell.DEAD => {
+                        if (live_neighbors == 3) {
+                            next_cell = Cell.ALIVE;
+                        }
+                    },
+                }
+
+                next_generation[idx] = next_cell;
+            }
+        }
+
+        for (next_generation, 0..) |c, i| {
+            self.cells[i] = c;
+        }
+    }
+
+    pub fn run(self: *Self) !void {
         for (0..self.generations) |_| {
             self.print_game();
             sleep(self.delay);
+            try self.evolve();
             self.current_generation += 1;
         }
     }
--- zig/src/main.zig
+++ zig/src/main.zig
@@ -7,69 +7,21 @@ const Cell = conway.Cell;
 const Coordinate = conway.Coordinate;
 
 pub fn main() !void {
-    const init = comptime [_]Coordinate{ .{ .x = 1, .y = 0 }, .{ .x = 1, .y = 1 }, .{ .x = 1, .y = 2 } };
-    // Initialize a 3 * 3 array of DEAD Cells
-    var cells: [3 * 3]Cell = .{Cell.DEAD} ** 9;
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    const game = try Conway.init(allocator, 3, 3, &init, "jee", 10, 500, &cells);
+    defer _ = gpa.deinit();
 
-    game.run();
+    const init_len: comptime_int = 3;
+    const init: [init_len]Coordinate = [init_len]Coordinate{ .{ .x = 1, .y = 0 }, .{ .x = 1, .y = 1 }, .{ .x = 1, .y = 2 } };
+
+    // Initialize a 3 * 3 array of DEAD Cells
+    const cells_len: comptime_int = 9;
+    var cells: [cells_len]Cell = .{Cell.DEAD} ** cells_len;
+    const ms: u32 = 500;
+
+    const game = try Conway.init(allocator, 3, 3, &init, "jee", 10, ms, &cells);
+
+    try game.run();
+
+    allocator.destroy(game);
 }
-
-// const a = [9]u16{ 0, 1, 0, 0, 1, 0, 0, 1, 0 };
-// const width = 3;
-// const height = 3;
-// _ = height; // autofix
-// var b: u16 = 1;
-// const ns_to_s: u32 = 1000000000;
-
-// try clear_screen();
-
-// inline for (0.., a) |i, elem| {
-//     print(if (elem == 1) "♥" else "‧", .{});
-//     if ((i + 1) % width == 0) {
-//         print("\n", .{});
-//     }
-// }
-
-// print("Generation: {}\n", .{b});
-
-// b += 1;
-
-// sleep(1 * ns_to_s);
-
-// try clear_screen();
-
-// inline for (0.., a) |i, elem| {
-//     print(if (elem == 1) "♥" else "‧", .{});
-//     if ((i + 1) % width == 0) {
-//         print("\n", .{});
-//     }
-// }
-
-// print("Generation: {}\n", .{b});
-
-// pub fn main() !void {
-//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-//     // stdout is for the actual output of your application, for example if you
-//     // are implementing gzip, then only the compressed bytes should be sent to
-//     // stdout, not any debugging messages.
-//     const stdout_file = std.io.getStdOut().writer();
-//     var bw = std.io.bufferedWriter(stdout_file);
-//     const stdout = bw.writer();
-
-//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-//     try bw.flush(); // don't forget to flush!
-// }
-
-// test "simple test" {
-//     var list = std.ArrayList(i32).init(std.testing.allocator);
-//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-//     try list.append(42);
-//     try std.testing.expectEqual(@as(i32, 42), list.pop());
-// }
